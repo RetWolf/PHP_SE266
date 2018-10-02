8ab30e02371f63acc09bceedce9abaaaa60a8cb7
Matt Conway <mattconway55@gmail.com>
2018-10-02 11:39:56 -0400

    Finished up initial run at Credit Card Interest assignment

--- Project/public/week1/creditcard/creditcard_form.php
+++ Project/public/week1/creditcard/creditcard_form.php
@@ -9,11 +9,48 @@
 </head>
 <body>
   <div class="container">
-    <form action="action.php" method="POST">
-      <label>Your name: <input type="text" name="name"></label>
-      <label>Your age: <input type="text" name="age"></label>
-      <button type="submit">Submit</button>
+    <form action="creditcard_form.php" method="POST">
+      <label>Amount Owed: <input type="text" name="amountOwed" value="<?php echo isset($_POST['amountOwed']) ? $_POST['amountOwed'] : "" ?>"></label>
+      <label>Interest Rate: <input type="text" name="interestRate" value="<?php echo isset($_POST['interestRate']) ? $_POST['interestRate'] : "" ?>"></label>
+      <label>Monthly Payment: <input type="text" name="monthlyPayment" value="<?php echo isset($_POST['monthlyPayment']) ? $_POST['monthlyPayment'] : "" ?>"</label>
+      <button type="submit">Calculate Damage</button>
     </form>
+  </div>
+  <div class="container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Month</th>
+          <th>Interest Paid</th>
+          <th>Owed</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php
+        if(isset($_POST['amountOwed'])) {
+          $startingAmount = $_POST['amountOwed'];
+          $month = 0;
+          $interestPaid = 0;
+          $totalInterest = 0;
+          while ($_POST['amountOwed'] > 0) {
+            $month++;
+            $interestPaid = round($interestPaid = $_POST['amountOwed'] * $_POST['interestRate'] / 100 / 12, 2);
+            $_POST['amountOwed'] = round($_POST['amountOwed'] += $interestPaid - $_POST['monthlyPayment'], 2);
+            $totalInterest += $interestPaid;
+            echo(
+            "<tr>
+              <td>{$month}</td>
+              <td>\${$interestPaid}</td>"
+              .($_POST['amountOwed'] > 0 ? "<td>\${$_POST['amountOwed']}</td>" : "<td></td>").
+            "</tr>"
+            );
+          }
+          $totalCost = round($startingAmount + $totalInterest, 2);
+        }
+      ?>
+      </tbody>
+    </table>
+    <?php echo("<p>The total cost over {$month} months was \${$totalCost}</p>"); ?>
   </div>
 </body>
 </html>
